f4a8f1ad0db3742dbf096da03b79d9be54a13b7a
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-10-02 14:57:24 +0200

    Add JsonArgumentsProvider with Annotation

---
+++ src/main/java/de/rpr/junit/jupiter/params/provider/JsonArgumentsProvider.java
@@ -0,0 +1,34 @@
+package de.rpr.junit.jupiter.params.provider;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.support.AnnotationConsumer;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class JsonArgumentsProvider implements ArgumentsProvider, AnnotationConsumer<JsonSource> {
+
+  private String[] value;
+  private Type type;
+
+  @Override
+  public void accept(final JsonSource annotation) {
+    value = annotation.value();
+    type = annotation.type();
+  }
+
+  @Override
+  public Stream<? extends Arguments> provideArguments(final ExtensionContext context) throws Exception {
+    return Arrays.stream(value)
+        .map(this::map)
+        .map(Arguments::of);
+  }
+
+  private Object map(final String value) {
+    return new Gson().fromJson(value, type);
+  }
+}
